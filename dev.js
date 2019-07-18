e3c72a8a61a19b18241d6aa40f6c0fbdadac5afe
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2019-07-18 14:40:00 +0200

    Scrape CSS-in-JS, reduce dependencies, normalize url (#10)

--- dev.js
+++
@@ -1,6 +0,0 @@
-const http = require('http')
-const extractCssFromUrl = require('.')
-
-http.createServer(extractCssFromUrl).listen(1337, () => {
-	console.log(`Server started at http://localhost:1337`)
-})
