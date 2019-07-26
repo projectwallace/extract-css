285b39501f6d5be92cf2ec9d2cf9c5850bf99fb7
Bart Veneman <bartveneman@users.noreply.github.com>
2019-07-26 20:40:47 +0200

    validate url before passing to puppeteer or got

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const got = require('got')
 const chromium = require('chrome-aws-lambda')
 const normalizeUrl = require('normalize-url')
+const isUrl = require('is-url')
 
 const extractCss = async url => {
 	const browser = await chromium.puppeteer.launch({
 		executablePath: await chromium.executablePath,
 		args: chromium.args,
-		headless: chromium.headless
+		headless: true
 	})
 
 	const page = await browser.newPage()
 
-	// // Start CSS coverage. This is the meat and bones of this module
+	// Start CSS coverage. This is the meat and bones of this module
 	await page.coverage.startCSSCoverage()
 
 	const response = await page.goto(url, { waitUntil: 'networkidle2' })
@@ -23,15 +24,13 @@ const extractCss = async url => {
 
 		return Promise.reject(
 			new Error(
-				`There was an error retrieving CSS from ${url}.\n\tHTTP status code: ${
-					response.statusCode
-				} (${response.statusText})`
+				`There was an error retrieving CSS from ${url}.\n\tHTTP status code: ${response.statusCode} (${response.statusText})`
 			)
 		)
 	}
 
-	// // Coverage contains a lot of <style> and <link> CSS,
-	// // but not all...
+	// Coverage contains a lot of <style> and <link> CSS,
+	// but not all...
 	const coverage = await page.coverage.stopCSSCoverage()
 
 	// Get all CSS generated with the CSSStyleSheet API
@@ -67,15 +66,27 @@ const extractCss = async url => {
 module.exports = async (req, res) => {
 	const url = normalizeUrl(req.url.slice(1), { stripWWW: false })
 
+	if (!isUrl(url)) {
+		res.statusCode = 406
+		res.setHeader('Content-Type', 'application/json')
+		return res.end(
+			JSON.stringify({
+				message: 'The provided URL is not valid'
+			})
+		)
+	}
+
 	try {
 		const css = url.endsWith('.css')
 			? (await got(url)).body
 			: await extractCss(url)
 
 		res.setHeader('Content-Type', 'text/css')
-
-		return res.status(200).send(css)
+		res.statusCode = 200
+		return res.end(css)
 	} catch (error) {
-		return res.status(400).json(error)
+		res.statusCode = 500
+		res.setHeader('Content-Type', 'application/json')
+		return res.end(JSON.stringify(error))
 	}
 }
