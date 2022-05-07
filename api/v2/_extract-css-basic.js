b9e9d0c97651322baf118ecf6a117225aba33d75
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2022-05-08 00:43:42 +0200

    Add v2 extract-css endpoint

---
+++ api/v2/_extract-css-basic.js
@@ -0,0 +1,124 @@
+import got from 'got'
+import { DOMParser } from 'linkedom'
+import _resolveUrl from '@jridgewell/resolve-uri'
+import parse from 'css-tree/parser'
+import walk from 'css-tree/walker'
+
+// To maintain query params, because resolve-uri strips them
+function resolveUrl(url, base) {
+  var resolved = _resolveUrl(url, base)
+
+  if (url.includes('?')) {
+    var search = url.substring(url.indexOf('?'))
+    return resolved + search
+  }
+
+  return resolved
+}
+
+function getImportUrls(css) {
+  var ast = parse(css, {
+    parseAtRulePrelude: false,
+    parseRulePrelude: false,
+    parseValue: false,
+    parseCustomProperty: false,
+  })
+  var urls = []
+
+  walk(ast, function (node) {
+    if (node.type === 'Url' && this.atrule?.name === 'import') {
+      urls.push(node.value)
+    }
+  })
+  return urls
+}
+
+export async function getCssFile(url) {
+  try {
+    var { body } = await got(url)
+    return body
+  } catch (error) {
+    console.error(`CSS not found at ${url} (HTTP ${error.response.statusCode})`)
+    console.error(error.message)
+    return ''
+  }
+}
+
+function getStyleNodes(html) {
+  var document = new DOMParser().parseFromString(html, 'text/html')
+  return document.querySelectorAll('link[rel*="stylesheet"][href], style, [style]')
+}
+
+export function getStyles(nodes) {
+  var items = []
+
+  for (var node of nodes) {
+    if (node.nodeName === 'LINK') {
+      items.push({
+        type: 'link',
+        href: node.getAttribute('href'),
+        media: node.getAttribute('media'),
+        rel: node.getAttribute('rel'),
+      })
+    }
+    if (node.nodeName === 'STYLE') {
+      var css = node.textContent
+      items.push({
+        type: 'style',
+        css,
+      })
+    }
+    if (node.hasAttribute('style')) {
+      items.push({
+        type: 'inline',
+        // using :where() to keep specificity 0 (but complexity += 2 here)
+        css: `:where([x-inline]) { ${node.getAttribute('style')} }`
+      })
+    }
+  }
+
+  return items
+}
+
+export async function extractCss(url) {
+  var { body } = await got(url)
+  var nodes = getStyleNodes(body)
+  var items = getStyles(nodes)
+  var result = []
+
+  for (let i = 0; i < items.length; i++) {
+    var item = items[i];
+
+    if (item.type === 'link') {
+      var fileUrl = resolveUrl(item.href, url)
+      var css = await getCssFile(fileUrl)
+      result.push({
+        ...item,
+        css
+      })
+    }
+
+    if (item.type === 'inline' || item.type === 'style') {
+      result.push(item)
+    }
+
+    if (item.type === 'style' || item.type === 'link') {
+      // Resolve @import CSS 1 level deep (to avoid infinite loops)
+      // And c'mon, don't @import inside your @import.
+      var importUrls = getImportUrls(item.css)
+      if (importUrls.length > 0) {
+        var cssRequests = importUrls.map(importUrl => getCssFile(resolveUrl(importUrl, url)))
+        var importedFiles = await Promise.all(cssRequests)
+        importedFiles.map((css, index) => {
+          result.push({
+            type: 'import',
+            css,
+            href: importUrls[index]
+          })
+        })
+      }
+    }
+  }
+
+  return result
+}
---
+++ api/v2/extract-css.js
@@ -0,0 +1,32 @@
+import { isUrl } from '../_is-url.js'
+import { extractCss } from './_extract-css-basic.js'
+
+export default async (req, res) => {
+  const { url } = req.query
+
+  if (!isUrl(url)) {
+    res.statusCode = 400
+
+    return res.send({
+      message: `The provided URL \`${url}\` is not valid`
+    })
+  }
+
+  try {
+    const result = await extractCss(url)
+
+    res.statusCode = 200
+    res.setHeader('Cache-Control', 'max-age=60')
+
+    if (req.headers.accept.includes('application/json')) {
+      return res.json(result)
+    }
+
+    res.setHeader('Content-Type', 'text/css')
+    const css = result.map(({ css }) => css).join('\n')
+    return res.end(css)
+  } catch (error) {
+    res.statusCode = 500
+    return res.json({ message: error.message })
+  }
+}
