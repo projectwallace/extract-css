e652c2561ea68aa870e1ab4aeedf770e11711fb6
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2022-05-08 00:44:42 +0200

    rm lru-cache from v1 endpoint

--- api/extract-css.js
+++ api/extract-css.js
@@ -1,14 +1,8 @@
-import LRU from 'lru-cache'
-import {extractCss} from './_chromium.js'
-import {isUrl} from './_is-url.js'
-
-const cssCache = new LRU({
-	max: 1000,
-	maxAge: 60 * 1000 // 60 seconds
-})
+import { extractCss } from './_chromium.js'
+import { isUrl } from './_is-url.js'
 
 export default async (req, res) => {
-	const {url} = req.query
+	const { url } = req.query
 
 	if (!isUrl(url)) {
 		res.statusCode = 400
@@ -18,33 +12,20 @@ export default async (req, res) => {
 		})
 	}
 
-	res.statusCode = 200
-
-	if (cssCache.has(url)) {
-		const result = cssCache.get(url)
-
-		if (req.headers.accept === 'application/json') {
-			return res.json(result)
-		}
-
-		res.setHeader('Content-Type', 'text/css')
-		const css = result.map(({css}) => css).join('\n')
-		return res.end(css)
-	}
-
 	try {
 		const result = await extractCss(url)
-		cssCache.set(url, result)
+
+		res.statusCode = 200
 
 		if (req.headers.accept === 'application/json') {
 			return res.json(result)
 		}
 
 		res.setHeader('Content-Type', 'text/css')
-		const css = result.map(({css}) => css).join('\n')
+		const css = result.map(({ css }) => css).join('\n')
 		return res.end(css)
 	} catch (error) {
 		res.statusCode = 500
-		return res.json({message: error.message})
+		return res.json({ message: error.message })
 	}
 }
